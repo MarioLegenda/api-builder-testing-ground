83862b76b5971dd0cc4cd211466638bfb3d61f02
MarioLegenda <whitepostmail@gmail.com>
2016-09-27 17:27:05 +0200

    Currencies can now be dynamically added

--- src/FindingAPI/Core/Information/Currency.php
+++ src/FindingAPI/Core/Information/Currency.php
@@ -2,6 +2,8 @@
 
 namespace FindingAPI\Core\Information;
 
+use FindingAPI\Core\Exception\ItemFilterException;
+
 class Currency
 {
     const AUSTRALIAN = 'aud';
@@ -18,4 +20,61 @@ class Currency
     const SWEDISH = 'sek';
     const TAIWAN = 'twd';
     const USA = 'usd';
+    /**
+     * @var array $currencies
+     */
+    private $currencies = array(
+        'aud',
+        'cad',
+        'chf',
+        'cny',
+        'eur',
+        'gbp',
+        'hkd',
+        'inr',
+        'myr',
+        'php',
+        'pln',
+        'sek',
+        'twd',
+        'usd',
+    );
+
+    /**
+     * @var GlobalId $instance
+     */
+    private static $instance;
+    /**
+     * @return GlobalId
+     */
+    public static function instance()
+    {
+        self::$instance = (self::$instance instanceof self) ? self::$instance : new self();
+
+        return self::$instance;
+    }
+    /**
+     * @param string $id
+     * @return mixed
+     */
+    public function hasId(string $currency) : bool
+    {
+        return in_array($currency, $this->currencies) !== false;
+    }
+    /**
+     * @param string $name
+     * @param array $values
+     * @return GlobalId
+     * @throws ItemFilterException
+     */
+    public function addId(string $currency)
+    {
+        if (!$this->hasId($currency)) {
+            return null;
+        }
+
+        $this->currencies[] = $currency;
+
+        return $this;
+    }
 }
