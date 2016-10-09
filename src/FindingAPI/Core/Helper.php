4eb53e4706c54c6ff311b08e8ad47e5c625a6229
MarioLegenda <whitepostmail@gmail.com>
2016-10-09 13:15:18 +0200

    Removed saved code and remove Carbon library

--- src/FindingAPI/Core/Helper.php
+++ src/FindingAPI/Core/Helper.php
@@ -96,44 +96,5 @@ class Helper
     public static function convertToGMT(\DateTime $dateTime)
     {
         return gmdate('Y-m-d\TH:i:s.', $dateTime->getTimestamp()).substr($dateTime->getTimestamp(), 0, 3).'Z';
-        $dateTime = Carbon::parse('2019-01-01 19:09:02');
-
-        $currentTime = explode(" ", microtime());
-        $currentTime = $currentTime[1];
-        $futureTime = $dateTime->getTimestamp(); // insert your date here
-        $microtime = ($futureTime - $currentTime) / 1000;
-        var_dump($microtime);
-        var_dump(microtime(true));
-        var_dump(microtime());
-        die();
-        $tMicro = sprintf("%03d", ($microtime - floor($microtime)) * 1000);
-
-
-        return $gmtDate;
-    }
-
-    public static function udate($format, $timestamp=null) {
-        $dateTime = new \DateTime('1.1.2019 13:23:48');
-
-
-        var_dump($timeRemaining);
-        die();
-
-        // treba mi gmdate
-
-        if (!isset($timestamp)) $timestamp = microtime();
-        // microtime(true)
-        if (count($t = explode(" ", $timestamp)) == 1) {
-            list($timestamp, $usec) = explode(".", $timestamp);
-            $usec = "." . $usec;
-        }
-        // microtime (much more precise)
-        else {
-            $usec = $t[0];
-            $timestamp = $t[1];
-        }
-        // 7 decimal places for "u" is maximum
-        $date = new \DateTime(date('Y-m-d H:i:s' . substr(sprintf('%.7f', $usec), 1), $timestamp));
-        return $date->format($format);
     }
 }
