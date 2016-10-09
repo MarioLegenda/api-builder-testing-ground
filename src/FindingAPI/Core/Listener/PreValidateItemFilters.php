41d48e52167179e7102f4e937803ae69aab161dd
MarioLegenda <whitepostmail@gmail.com>
2016-10-09 18:40:32 +0200

    Validated LocalSearchOnly special conditions

--- src/FindingAPI/Core/Listener/PreValidateItemFilters.php
+++ src/FindingAPI/Core/Listener/PreValidateItemFilters.php
@@ -20,11 +20,24 @@ class PreValidateItemFilters
         if (count($foundFilters) > 1) {
             throw new ItemFilterException('The ExcludeSeller item filter cannot be used together with either the Seller or TopRatedSellerOnly item filters or vice versa');
         }
-        
+
         $foundFilters = $itemFilterStorage->getItemFiltersInBulk(array('AvailableTo', 'LocatedIn'), true);
-        
+
         if (count($foundFilters) > 1) {
             throw new ItemFilterException('AvailableTo item filter cannot be used together with LocatedIn item filter and vice versa');
+        }
+
+        if ($itemFilterStorage->hasItemFilter('LocalSearchOnly')) {
+            $localSearchOnly = $itemFilterStorage->getItemFilter('LocalSearchOnly');
+
+            if ($localSearchOnly['value'] !== null) {
+                $maxDistance = $itemFilterStorage->getItemFilter('MaxDistance');
+                $buyerPostalCode = $itemFilterStorage->getItemFilter('BuyerPostalCode');
+
+                if ($maxDistance['value'] === null or $buyerPostalCode['value'] === null) {
+                    throw new ItemFilterException('LocalSearchOnly item filter has to be used together with MaxDistance item filter and buyerPostalCode');
+                }
+            }
         }
     }
 }
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -54,6 +54,7 @@ class MainTest extends \PHPUnit_Framework_TestCase
 
         $request
             ->setOperationName('findItemsByKeywords')
+            ->setBuyerPostalCode(31000)
             ->setSortOrder(SortOrder::START_TIME_NEWEST)
             ->setPaginationInput(20, 'pageNumber')
             //->addOption(Options::SMART_GUESS_SYSTEM)
@@ -79,12 +80,12 @@ class MainTest extends \PHPUnit_Framework_TestCase
 //            ->addItemFilter(ItemFilter::LISTED_IN, array(GlobalId::EBAY_AT))
 //            ->addItemFilter(ItemFilter::LISTING_TYPE, array('All', 'AuctionWithBIN'))
 //            ->addItemFilter(ItemFilter::LOCAL_PICKUP_ONLY, array(true))
-//            ->addItemFilter(ItemFilter::LOCAL_SEARCH_ONLY, array(true))
+            ->addItemFilter(ItemFilter::LOCAL_SEARCH_ONLY, array(true))
 //            ->addItemFilter(ItemFilter::LOCATED_IN, array('dz', 'ai'))
 //            ->addItemFilter(ItemFilter::LOTS_ONLY, array(true))
             ->addItemFilter(ItemFilter::MAX_BIDS, array(1))
 //            ->addItemFilter(ItemFilter::MAX_HANDLING_TIME, array(1))
-//            ->addItemFilter(ItemFilter::MAX_DISTANCE, array(6))
+            ->addItemFilter(ItemFilter::MAX_DISTANCE, array(6))
 //            ->addItemFilter(ItemFilter::MAX_PRICE, array(0.0, Currency::AUSTRALIAN))
 //            ->addItemFilter(ItemFilter::MAX_QUANTITY, array(1))
             ->addItemFilter(ItemFilter::MIN_BIDS, array(0))
