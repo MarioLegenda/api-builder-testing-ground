36741cfa78570c0d2b2e7b1bbe7f3c525f068156
Mario Skrlec <whitepostmail@gmail.com>
2017-01-01 10:47:10 +0100

    Commit before going to previous commit

--- demo/Demo/TwigBridge.php
+++ demo/Demo/TwigBridge.php
@@ -2,7 +2,32 @@
 
 namespace Demo;
 
+require_once __DIR__.'/../../vendor/twig/twig/lib/Twig/Autoloader.php';
+
 class TwigBridge
 {
+    /**
+     * @var \Twig_Environment $twig
+     */
+    private $twig;
 
+    public function __construct()
+    {
+        \Twig_Autoloader::register();
+
+        $loader = new \Twig_Loader_Filesystem('/path/to/templates');
+        $twig = new \Twig_Environment($loader, array(
+            'cache' => false,
+            'strict_variables' => true,
+        ));
+
+        $this->twig = $twig;
+    }
+    /**
+     * @return \Twig_Environment
+     */
+    public function getTwig() : \Twig_Environment
+    {
+        return $this->twig;
+    }
 }
