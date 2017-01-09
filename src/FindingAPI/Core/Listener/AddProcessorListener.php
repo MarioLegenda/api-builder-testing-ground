eae77a565581ca43c41e6527392b655aa3968b6b
Mario Skrlec <whitepostmail@gmail.com>
2017-01-09 18:27:36 +0100

    Removed adding dynamic processor in finding api and moved it into sdk

--- src/FindingAPI/Core/Listener/AddProcessorListener.php
+++ src/FindingAPI/Core/Listener/AddProcessorListener.php
@@ -14,18 +14,6 @@ class AddProcessorListener
         $processorFactory = $event->getProcessorFactory();
         $request = $event->getRequest();
 
-        if ($request->getMethod() === 'get') {
-            $processorFactory->registerCallbackProcessor($request->getMethod(), function(RequestInterface $request) {
-                $itemFilterStorage = $request->getDynamicStorage();
-
-                if (!empty($itemFilterStorage)) {
-                    if ($request->getMethod() === 'get') {
-                        return new GetItemFiltersProcessor($request, $itemFilterStorage);
-                    }
-                }
-            });
-        }
-
         if ($request->getMethod() === 'post') {
             $processorFactory->registerProcessor($request->getMethod(), PostRequestXmlProcessor::class);
         }
